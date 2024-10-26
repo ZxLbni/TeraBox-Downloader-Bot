8effa9b26828b0f9f143fa10ab32b3282b49a2d5
Noob <101978891+ZxLbni@users.noreply.github.com>
2024-10-26 15:35:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,25 @@
 # API credentials
-API_ID = 123456
-API_HASH = "DWSC65HGFswcf56g8ergjhgdw54586"
-BOT_TOKEN = "123456:abcdekgfiJLKMNOPqjngghdf"
+API_ID = 22419004
+API_HASH = "34982b52c4a83c2af3ce8f4fe12fe4e1"
+BOT_TOKEN = "7718397631:AAEIEGqpOFgmwxWOcJ1Kqw7ionmtSAVZTX8"
 
 # Redis configuration
 REDIS_CONFIG = {
-    "HOST": "redis-12345.c16.us-east-1-2.ec2.cloud.redislabs.com",
-    "PORT": 17713,
+    "HOST": "redis-17607.c273.us-east-1-2.ec2.cloud.redislabs.com",
+    "PORT": 17607,
     "USERNAME": "default",
-    "PASSWORD": "password",
-    "DATABASE": "NTM"
+    "PASSWORD": "hoNgOG8I27Hw19YvTFFIoyqJHV0HD2lo",
+    "DATABASE": "TBX"
 }
 
 # Chat ID where videos will be stored
-PRIVATE_CHAT_ID = -100123456789
+PRIVATE_CHAT_ID = -1002445495902
 
 # Authentication cookie
 COOKIE = {
-    "COOKIE": "Paste your cookie here"
+    "COOKIE": "csrfToken=m_zbAV1arBvXCvTgmFvSluXY; browserid=EAaVwqpE6IPuBWAbOmlwEa-HARfMNQSSWLn9wiOXV1pWAEArqSzqtnmnkqM=; lang=en; TSID=RgCB6tEItxCB33EMPKyzutFSBMmUxfjl; __bid_n=190fa81594548da7784207; _ga=GA1.1.1547073603.1722189820; ndus=YuaYNCMteHuiIaOZqGpTf8Z9-n1Si4erYpgWHcaX; ab_sr=1.0.1_ODA4NTRlNzE0MjVkYzczZjZjNmQ3NGI0MzZjNTIxNGI1MThmNjdmZTNlM2I1ZmU5N2E0NmY5Mjg2OTdjMDg5NzM2NDhiZWRlNGFjY2RiZTNhNzQ2YmEwNzAwNzJhZGQ3ZDNiOTY3ZjNjYjVhZGZiODA4ZWU5ZDUxZjZkMjk0NTYzOWYyYzVkZDFhZmRmM2RhNjUyNDA5YTBjZjk0MDVlZg==; ndut_fmt=426D9C20125DC8DFB9349A68EB0F767398283E07567B9C028271312D315A284E; _ga_06ZNKL8C2E=GS1.1.1722189819.1.1.1722191121.52.0.0"
 }
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 
 # List of bot admins
-ADMINS = [803003146]
+ADMINS = [6742022802,7442532306]
